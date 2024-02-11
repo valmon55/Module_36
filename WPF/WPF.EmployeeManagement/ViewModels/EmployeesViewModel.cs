1ed2dbd8d91c35a2a460462f772c0c7ec1df2d03
valmon55 <fedor_ka@mail.ru>
2024-02-11 14:03:18 +0300

    Привязки. почему не работает?

---
+++ WPF/WPF.EmployeeManagement/ViewModels/EmployeesViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using WPF.EmployeeManagement.Model;
+
+namespace WPF.EmployeeManagement.ViewModels
+{
+    public class EmployeesViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        private EmployeeRepository _employeeRepository;
+        private string _filter;
+        public string Filter 
+        {
+            get { return _filter; }    
+            set { _filter = value; }
+        }
+        public EmployeesViewModel()
+        { 
+            _employeeRepository= new EmployeeRepository();
+            FillListView();
+        }
+        public ObservableCollection<Employee> _employees;
+
+        public ObservableCollection<Employee> Employees
+        { 
+            get
+            {
+                return _employees;
+            }
+            set 
+            { 
+                _employees = value; 
+                OnPropertyChanged();
+            }
+        } 
+        private void FillListView()
+        {
+            if(!String.IsNullOrEmpty(Filter))
+            {
+                _employees= new ObservableCollection<Employee>( _employeeRepository.GetAll().Where(x => x.FirstName.Contains(_filter)));
+            }
+            else 
+            {
+                _employees = new ObservableCollection<Employee>(_employeeRepository.GetAll());
+            }
+        }
+    }
+}
---
+++ WPF/WPF.EmployeeManagement/Views/EmployeesView.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WPF.EmployeeManagement.Model;
+
+namespace WPF.EmployeeManagement.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для EmployeesView.xaml
+    /// </summary>
+    public partial class EmployeesView : Window
+    {
+        public EmployeesView()
+        {
+            InitializeComponent();
+        }
+
+        private void ListView_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var item = (sender as ListView).SelectedItem;
+            if (item is null) 
+            {
+                return;
+            }
+
+            var employee = item as Employee;
+
+            MessageBox.Show($"{employee.LastName} \n\r {employee.FirstName} \n\r {employee.Age.ToString()} \n\r" +
+                $"{employee.Position} \n\r {employee.CompanyName} \n\r {employee.CityName}");
+        }
+    }
+}
